498acea13c78c025836a1c99f5ff13fd1c36f6ca
Ryan Cardona <rookie.ry9@eagles.ewu.edu>
2018-02-18 00:32:31 -0800

    Add files via upload

---
+++ SparkAPI/Models/Checkout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkAPI.Models
+{
+    public class Checkout
+    {
+        public int ItemId { get; set; }
+        public int MemberId { get; set; }
+        public String ItemType { get; set; }
+        public DateTime dueDate { get; set; }
+    }
+}
---
+++ SparkAPI/Models/Fines.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkAPI.Models
+{
+    public class Fines
+    {
+        public int memberId { get; set; }
+        public float amount { get; set; }
+    }
+}
